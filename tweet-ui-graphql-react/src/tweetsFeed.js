75f630e6e31c203805aadf46a84035579c19c352
Caleb Sattgast <csattgast@cloudentity.com>
2022-02-07 15:48:58 -0800

    Edit and delete operations for tweets

--- tweet-ui-graphql-react/src/tweetsFeed.js
+++ tweet-ui-graphql-react/src/tweetsFeed.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import './index.css';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import CancelIcon from '@mui/icons-material/Cancel';
+import ConfirmIcon from '@mui/icons-material/CheckCircle';
+import TextField from "@mui/material/TextField";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 import {
+  useMutation,
   useQuery,
   gql
 } from "@apollo/client";
@@ -27,10 +41,18 @@ query {
   }
 `;
 
+const DELETE_TWEET_MUTATION = gql`
+mutation DeleteTweetMutation(
+    $id: String!
+  ) {
+    deleteTweet(id: $id)
+  }
+`;
+
 function isError403(e)  {
    // console.log("Checkinf if the network error is a 403");
     if(e.networkError != null) {
-        
+
         switch(e.networkError.statusCode) {
             case 403:
                 return true;
@@ -48,7 +70,55 @@ function isError403(e)  {
 export function GetLatestTweets() {
   const {loading, error, data} = useQuery(FETCH_LATEST_TWEETS);
 
-  const  [latestTweetFetchStatus, setFetchLatestTweetsStatus] = useState(false);
+  const [latestTweetFetchStatus, setFetchLatestTweetsStatus] = useState(false);
+  const [currentlyEditing, setCurrentlyEditing] = useState('');
+  const [editText, setEditText] = useState('');
+  const [currentlyDeleting, setCurrentlyDeleting] = useState('')
+  const [confirmDeleteDialogOpen, setConfirmDeleteDialogOpen] = useState(false);
+
+  const handleStartEdit = (id, content) => {
+    setCurrentlyEditing(id);
+    setEditText(content);
+  }
+
+  const handleConfirmEdit = () => {
+    console.log('id to edit:', currentlyEditing);
+    console.log('content to edit:', editText);
+    // TODO: add edit mutation call, refresh list
+    setCurrentlyEditing('');
+    setEditText('');
+    window.location.reload(false);
+  }
+
+  const handleCancelEdit = () => {
+    setCurrentlyEditing('');
+    setEditText('');
+  }
+
+  const [deleteTweetMutation] =
+    useMutation(DELETE_TWEET_MUTATION, {
+      variables: {
+        id: currentlyDeleting
+      }
+    })
+
+  const handleDeleteTweet = (id) => {
+    console.log('Deleting this tweet id:', id);
+    setConfirmDeleteDialogOpen(true);
+    setCurrentlyDeleting(id);
+  }
+
+  const handleConfirmDelete = () => {
+    deleteTweetMutation();
+    setConfirmDeleteDialogOpen(false);
+    setCurrentlyDeleting('');
+    window.location.reload(false);
+  }
+
+  const handleCancelDelete = () => {
+    setConfirmDeleteDialogOpen(false);
+    setCurrentlyDeleting('');
+  }
 
   if (loading) return <p>Loading..</p>;
   if (error && isError403(error)) return (
@@ -56,14 +126,14 @@ export function GetLatestTweets() {
         <h2> Latest Tweets</h2>
         <p> Our systems have detected that this request is NOT authorized to see the tweets... </p>
         <p> Reason for unauthorized: </p>
-        
+
     </div>
 );
   if (error) return (
       <div>
           <h2> Latest Tweets</h2>
           <p> Unable to connect to GraphQL resource server ... </p>
-          
+
       </div>
   );
 
@@ -76,22 +146,78 @@ export function GetLatestTweets() {
     return (
       <div>
         <h2> Latest Tweets</h2>
-
+        <ul>
         {
           Object.keys(data.getLatestTweets).map((k,i) => (
-              <ul>
-                  <div id={data.getLatestTweets[i].id}> 
-                  <u> {data.getLatestTweets[i].dateCreated} : </u>
-                  {data.getLatestTweets[i].author} tweeted <i> " {data.getLatestTweets[i].content}  "</i>
-                 </div>
-                
-            </ul>
+            <div style={{marginTop: 15, display: 'flex', flexDirection: 'row'}} key={i} id={data.getLatestTweets[i].id}>
+            {currentlyEditing === data.getLatestTweets[i].id ? (
+              <Fragment>
+                <div>
+                  Edit this tweet:
+                </div>
+                <div style={{marginLeft: 15}}>
+                  <TextField value={editText} name="content" placeholder="" multiline rows={2} onChange={(e) => setEditText(e.target.value)}/>
+                </div>
+                <div style={{marginLeft: 10, marginTop: -12}}>
+                  <Stack direction="row" spacing={1}>
+                    <IconButton aria-label="cancel" onClick={handleConfirmEdit}>
+                      <ConfirmIcon />
+                    </IconButton>
+                    <IconButton aria-label="cancel" onClick={handleCancelEdit}>
+                      <CancelIcon />
+                    </IconButton>
+                  </Stack>
+                </div>
+              </Fragment>
+            ) : (
+              <Fragment>
+                <div>
+                  <u> {data.getLatestTweets[i].dateCreated} :</u>
+                  <span> {data.getLatestTweets[i].author} tweeted <i> " {data.getLatestTweets[i].content} "</i></span>
+                </div>
+                <div style={{marginLeft: 10, marginTop: -12}}>
+                  <Stack direction="row" spacing={1}>
+                    <IconButton aria-label="edit" onClick={() => handleStartEdit(data.getLatestTweets[i].id, data.getLatestTweets[i].content)}>
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton aria-label="delete" onClick={() => handleDeleteTweet(data.getLatestTweets[i].id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </Stack>
+                </div>
+              </Fragment>
+            )}
+            </div>
           ))
         }
+        </ul>
+        <div>
+          <Dialog
+            open={confirmDeleteDialogOpen}
+            onClose={handleCancelDelete}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              {"Delete this tweet?"}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                This tweet will be permanently deleted.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleCancelDelete}>Cancel</Button>
+              <Button onClick={handleConfirmDelete} autoFocus>
+                OK
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
       </div>
     );
   }
-  return <TweetsNotAvailable />;  
+  return <TweetsNotAvailable />;
 
 }
 
@@ -99,9 +225,9 @@ export function GetLatestTweets() {
 
 export function GetLatestTweetsForLoggedInUser() {
     const {loading, error, data} = useQuery(FETCH_LATEST_TWEETS);
-  
+
     const  [latestTweetFetchStatus, setFetchLatestTweetsStatus] = useState(false);
-  
+
     if (loading) return <p>Loading..</p>;
     if (error) return (
         <div>
@@ -113,32 +239,32 @@ export function GetLatestTweetsForLoggedInUser() {
             </pre>
         </div>
     );
-  
+
     if(!latestTweetFetchStatus && data != null && data.getLatestTweets != null) {
       setFetchLatestTweetsStatus(true);
     }
-  
+
     if(latestTweetFetchStatus) {
       return (
         <div>
           <h2> Latest Tweets</h2>
-  
+
           <p> Our records indicate that you have already exercised your ballot rights.
             In case you want to modify the ballot, click on modify below.
           </p>
-  
+
           {
             Object.keys(data.getLatestTweets).map((k,i) => (
               <div>Hi {i} {k} {data.getLatestTweets[i].id} </div>
             ))
           }
-  
-         
+
+
         </div>
       );
     }
-    return <TweetsNotAvailable />;  
-  
+    return <TweetsNotAvailable />;
+
   }
 
 
